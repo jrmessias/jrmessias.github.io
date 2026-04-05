9cf007b184edd4898ce6c4cf8442893963fc82f1
Júnior Messias <jrmessias@gmail.com>
2026-04-05 01:14:39 -0300

    Update skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -98,12 +98,34 @@ const skills = [
         '</svg>'
   },
   {
+    title: 'React',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="stroke-imj-500">' +
+        '<path stroke="none" d="M0 0h24v24H0z" fill="none" />' +
+        '<path d="M6.306 8.711c-2.602 .723 -4.306 1.926 -4.306 3.289c0 2.21 4.477 4 10 4c.773 0 1.526 -.035 2.248 -.102" />' +
+        '<path d="M17.692 15.289c2.603 -.722 4.308 -1.926 4.308 -3.289c0 -2.21 -4.477 -4 -10 -4c-.773 0 -1.526 .035 -2.25 .102" />' +
+        '<path d="M6.305 15.287c-.676 2.615 -.485 4.693 .695 5.373c1.913 1.105 5.703 -1.877 8.464 -6.66c.387 -.67 .733 -1.339 1.036 -2" />' +
+        '<path d="M17.694 8.716c.677 -2.616 .487 -4.696 -.694 -5.376c-1.913 -1.105 -5.703 1.877 -8.464 6.66c-.387 .67 -.733 1.34 -1.037 2" />' +
+        '<path d="M12 5.424c-1.925 -1.892 -3.82 -2.766 -5 -2.084c-1.913 1.104 -1.226 5.877 1.536 10.66c.386 .67 .793 1.304 1.212 1.896" />' +
+        '<path d="M12 18.574c1.926 1.893 3.821 2.768 5 2.086c1.913 -1.104 1.226 -5.877 -1.536 -10.66c-.375 -.65 -.78 -1.283 -1.212 -1.897" />' +
+        '<path d="M11.5 12.866a1 1 0 1 0 1 -1.732a1 1 0 0 0 -1 1.732" />' +
+        '</svg>'
+  },
+  {
     title: 'Vite',
     icon: '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="stroke-imj-500">' +
         '<path stroke="none" d="M0 0h24v24H0z" fill="none"/>' +
         '<path d="M10 4.5l6 -1.5l-2 6.5l2 -.5l-4 7v-5l-3 1z" />' +
         '<path d="M15 6.5l7 -1.5l-10 17l-10 -17l7.741 1.5" />' +
         '</svg>'
+  },
+  {
+    title: 'Figma',
+    icon: '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="stroke-imj-500">' +
+        '<path stroke="none" d="M0 0h24v24H0z" fill="none"/>' +
+        '<path d="M15 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />' +
+        '<path d="M6 3m0 3a3 3 0 0 1 3 -3h6a3 3 0 0 1 3 3v0a3 3 0 0 1 -3 3h-6a3 3 0 0 1 -3 -3z" />' +
+        '<path d="M9 9a3 3 0 0 0 0 6h3m-3 0a3 3 0 1 0 3 3v-15" />' +
+        '</svg>',
   },
   {
     title: 'CSS',
